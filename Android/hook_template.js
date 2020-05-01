be2fa76b7863161c8f4ae61820c3049aa3add5ab
Mobile Security <26333219+m0bilesecurity@users.noreply.github.com>
2020-05-01 20:19:32 +0200

    StackTrace info

--- Android/hook_template.js
+++ Android/hook_template.js
@@ -15,13 +15,17 @@ Java.perform(function () {
     var classname = "{className}"; 
     var classmethod = "{classMethod}";
     var hookclass = Java.use(classname);
+    var jlog = Java.use('android.util.Log')
+    var jexception = Java.use('java.lang.Exception');
+
     hookclass.{classMethod}.{overload}.implementation = function ({args}) {
         console.log("CALLED: " + classname + "." + classmethod + "()\\n");
         var ret = this.{classMethod}({args});
         var s="";
-        s=s+("HOOK: " + classname + "." + classmethod + "()\\n");
-        s=s+"Input: "+eval(args)+"\\n";
-        s=s+"Output: "+ret+"\\n";
+        s=s+"HOOK: " + classname + "." + classmethod + "()\\n";
+        s=s+"IN: "+eval(args)+"\\n";
+        s=s+"OUT: "+ret+"\\n";
+        s=s+"StackTrace: "+jlog.getStackTraceString(jexception.$new()) +"\\n";
         console.log(s);
                 
         return ret;
